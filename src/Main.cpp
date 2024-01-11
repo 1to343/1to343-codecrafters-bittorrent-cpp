cc545b4a740ea6461fb83bc1dde2089c5b69a312
1to343 <isomatov.shm@pphystech.edu>
2024-01-11 19:10:41 +0500

    pass 1sst stage

--- src/Main.cpp
+++ src/Main.cpp
@@ -23,8 +23,13 @@ bool is_encoded_list(const std::string& encoded_value) {
          encoded_value[encoded_value.size() - 1] == 'e';
 }
 
+bool is_encoded_dict(const std::string& encoded_value) {
+  return encoded_value[0] == 'd' &&
+         encoded_value[encoded_value.size() - 1] == 'e';
+}
+
 json decode_bencoded_string(const std::string& encoded_value, uint& index) {
-  std::cout << index << " str\n";
+//  std::cout << index << " str\n";
   size_t colon_index = encoded_value.find(':', index);
   if (colon_index != std::string::npos) {
     std::string number_string =
@@ -39,7 +44,7 @@ json decode_bencoded_string(const std::string& encoded_value, uint& index) {
 }
 
 json decode_bencoded_number(const std::string& encoded_value, uint& index) {
-  std::cout << index << " num\n";
+//  std::cout << index << " num\n";
   uint last = encoded_value.find('e', index);
   const std::string number_str =
       encoded_value.substr(index + 1, last - index - 1);
@@ -49,7 +54,6 @@ json decode_bencoded_number(const std::string& encoded_value, uint& index) {
 }
 
 json decode_bencoded_list(const std::string& encoded_value, uint& index) {
-  std::cout << index << " list\n";
   json arr = json::array();
   while (encoded_value[index] != 'e') {
     json ans;
@@ -72,24 +76,39 @@ json decode_bencoded_list(const std::string& encoded_value, uint& index) {
 json decode_bencoded_dict(const std::string& encoded_value, uint& index) {
   json dict;
   bool done = false;
+  json first_val;
+  json second_val;
   while (encoded_value[index] != 'e') {
-    json first;
-    json second;
     if (encoded_value[index] == 'i') {
-      second = decode_bencoded_number(encoded_value, index);
+      second_val = decode_bencoded_number(encoded_value, index);
+      auto first = first_val.dump();
+      auto second = second_val.dump();
+      //      std::cout << "first and second " << first << ' ' << second <<
+      //      '\n';
       dict[first] = second;
       done = false;
     } else if (encoded_value[index] == 'l') {
-      second = decode_bencoded_list(encoded_value, index);
+      second_val = decode_bencoded_list(encoded_value, index);
+      auto first = first_val.dump();
+      auto second = second_val.dump();
+      //      std::cout << "first and second " << first << ' ' << second <<
+      //      '\n';
       dict[first] = second;
       done = false;
     } else {
       if (done) {
-        second = decode_bencoded_string(encoded_value, index);
+        second_val = decode_bencoded_string(encoded_value, index);
+        auto first = first_val.dump();
+        auto second = second_val.dump();
+        //        std::cout << "first and second " << first << ' ' << second <<
+        //        '\n';
         dict[first] = second;
         done = false;
       } else {
-        first = decode_bencoded_string(encoded_value, index);
+        first_val = decode_bencoded_string(encoded_value, index);
+        //        json tmp = first_val;
+        //        auto first = tmp.dump();
+        //        std::cout << "only first " << first << '\n';
         done = true;
       }
     }
@@ -108,47 +127,48 @@ json decode_bencoded_value(const std::string& encoded_value) {
     return decode_bencoded_number(encoded_value, index);
   } else if (is_encoded_list(encoded_value)) {
     return decode_bencoded_list(encoded_value, index);
+  } else if (is_encoded_dict(encoded_value)) {
+    return decode_bencoded_dict(encoded_value, index);
   } else {
     throw std::runtime_error("Unhandled encoded value: " + encoded_value);
   }
 }
-//
-// int main(int argc, char* argv[]) {
-//  if (argc < 2) {
-//    std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" <<
-//    std::endl; return 1;
-//  }
-//
-//  std::string command = argv[1];
-//
-//  if (command == "decode") {
-//    if (argc < 3) {
-//      std::cerr << "Usage: " << argv[0] << " decode <encoded_value>"
-//                << std::endl;
-//      return 1;
-//    }
-//    // You can use print statements as follows for debugging, they'll be
-//    visible
-//    // when running tests.
-//    //    std::cout << "Logs from your program will appear here!" <<
-//    std::endl;
-//
-//    // Uncomment this block to pass the first stage
-//    std::string encoded_value = argv[2];
-//    json decoded_value = decode_bencoded_value(encoded_value);
-//    std::cout << decoded_value.dump() << std::endl;
-//  } else {
-//    std::cerr << "unknown command: " << command << std::endl;
-//    return 1;
-//  }
-//
-//  return 0;
-//}
 
-int main() {
-  std::string value;
-  std::cin >> value;
-  json ans = decode_bencoded_value(value);
-  std::cout << ans.dump() << '\n';
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " decode <encoded_value>" << std::endl;
+    return 1;
+  }
+
+  std::string command = argv[1];
+
+  if (command == "decode") {
+    if (argc < 3) {
+      std::cerr << "Usage: " << argv[0] << " decode <encoded_value>"
+                << std::endl;
+      return 1;
+    }
+    // You can use print statements as follows for debugging, they'll be visible
+        // when running tests.
+        //    std::cout << "Logs from your program will appear here!" <<
+//        std::endl;
+
+    // Uncomment this block to pass the first_val stage
+    std::string encoded_value = argv[2];
+    json decoded_value = decode_bencoded_value(encoded_value);
+    std::cout << decoded_value.dump() << std::endl;
+  } else {
+    std::cerr << "unknown command: " << command << std::endl;
+    return 1;
+  }
+
+  return 0;
 }
-// ll5:helloi1ee4:shiti2ee
+
+// int main() {
+//   std::string value;
+//   std::cin >> value;
+//   json ans = decode_bencoded_value(value);
+//   std::cout << ans.dump() << '\n';
+// }
+//  ll5:helloi1ee4:shiti2ee
