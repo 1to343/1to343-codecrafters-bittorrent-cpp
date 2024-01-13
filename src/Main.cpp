930467189df3d8742a0ad075adc17c6fea7b2ae9
1to343 <isomatov.shm@pphystech.edu>
2024-01-13 17:04:41 +0500

    pass 1sst stage

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,6 @@
+#include <curl/curl.h>
+#include <openssl/sha.h>
+
 #include <cctype>
 #include <cstdlib>
 #include <fstream>
@@ -7,10 +10,8 @@
 #include <vector>
 
 #include "lib/nlohmann/json.hpp"
-#include "lib/sha1.hpp"
 
 using json = nlohmann::json;
-
 
 bool is_encoded_num(const std::string& encoded_value) {
   if (encoded_value[0] != 'i') {
@@ -164,11 +165,54 @@ std::string bencode_the_string(json info) {
   return ans;
 }
 
-std::string string_to_sha1(const std::string& data) {
-  SHA1 cipher;
-  cipher.update(data);
-  std::string ans = cipher.final();
-  return ans;
+void string_to_sha1(const std::string& data,
+                    std::array<unsigned char, SHA_DIGEST_LENGTH>& hash) {
+  SHA1(reinterpret_cast<const unsigned char*>(data.c_str()), data.size(),
+       hash.data());
+}
+
+void get_pieces_hashes(const json& info, std::vector<std::string>& res) {
+  std::string pieces_string = info["pieces"];
+  std::vector<std::string> pieces_hashes;
+  for (uint64_t i = 0; i < pieces_string.size(); i += 20) {
+    pieces_hashes.push_back(pieces_string.substr(i, 20));
+  }
+  std::stringstream ss;
+  for (const auto& hash : pieces_hashes) {
+    ss.str("");
+    for (unsigned char c : hash) {
+      ss << std::hex << std::setfill('0') << std::setw(2)
+         << static_cast<int>(c);
+    }
+    if (ss.str().length() != 40) {
+      throw std::runtime_error("Wrong piece hash length: " + ss.str());
+    }
+    res.push_back(ss.str());
+  }
+}
+
+std::vector<std::string> extract_info(const std::string& buffer) {
+  std::vector<std::string> res;
+  std::array<unsigned char, SHA_DIGEST_LENGTH> info_hash;
+  auto torrent = decode_bencoded_value(buffer);
+  std::string announce = torrent["announce"];
+  res.push_back("Tracker URL: " + announce);
+  auto info = torrent["info"];
+  std::string length = std::to_string(info["length"].template get<int>());
+  res.push_back("Length: " + length);
+  string_to_sha1(bencode_the_string(info), info_hash);
+  std::stringstream ss;
+  for (auto c : info_hash) {
+    ss << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(c);
+  }
+  std::string hexStr = ss.str();
+  res.push_back("Info Hash: " + ss.str());
+  std::string piece_length =
+      std::to_string(info["piece length"].template get<int>());
+  res.push_back("Piece Length: " + piece_length);
+  res.push_back("Pieces Hashes:");
+  get_pieces_hashes(info, res);
+  return res;
 }
 
 std::vector<std::string> parse_torrent_file(const std::string& filename) {
@@ -180,34 +224,64 @@ std::vector<std::string> parse_torrent_file(const std::string& filename) {
   }
   std::istreambuf_iterator<char> it{fs}, end;
   std::string buffer(it, end);
-  auto torrent = decode_bencoded_value(buffer);
-  std::string announce = torrent["announce"];
-  res.push_back("Tracker URL: " + announce);
-  auto info = torrent["info"];
-  std::string length = std::to_string(info["length"].template get<int>());
-  res.push_back("Length: " + length);
-  std::string info_hash = string_to_sha1(bencode_the_string(info));
-  res.push_back("Info Hash: " + info_hash);
-  std::string piece_length = std::to_string(info["piece length"].template get<int>());
-  res.push_back("Piece Length: " + piece_length);
-  res.push_back("Pieces Hashes:");
-  std::string pieces_string = info["pieces"];
-  std::vector<std::string> pieces_hashes;
-  for (uint64_t i = 0; i < pieces_string.size(); i+=20) {
-    pieces_hashes.push_back(pieces_string.substr(i, 20));
-  }
-  std::stringstream ss;
-  for (auto hash: pieces_hashes) {
-    ss.str("");
-    for (unsigned char c: hash) {
-      ss << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(c);
-    }
-    if (ss.str().length() != 40) {
-      throw std::runtime_error("Wrong piece hash length: " + ss.str());
-    }
-    res.push_back(ss.str());
-  }
+  res = extract_info(buffer);
   return res;
+}
+
+size_t write_callback(void* contents, size_t size, size_t nmemb,
+                      std::string* output) {
+  size_t totalSize = size * nmemb;
+  output->append(static_cast<char*>(contents), totalSize);
+  return totalSize;
+}
+
+void send_request(const std::vector<std::string>& res) {
+  CURL* curl = curl_easy_init();
+  std::array<unsigned char, SHA_DIGEST_LENGTH> hash;
+  if (!curl) {
+    std::cerr << "Failed to initialize cURL" << std::endl;
+    return;
+  }
+
+  std::string tracker = res[0].substr(13);
+  std::string info_hash = res[2].substr(12);
+  std::string peer_id = "00112233445566778899";
+  size_t port = 6881;
+  size_t uploaded = 0;
+  size_t downloaded = 0;
+  size_t left = std::stoul(res[1].substr(8));
+  size_t compact = 1;
+  std::string response;
+  SHA1(reinterpret_cast<const unsigned char*>(info_hash.c_str()),
+       info_hash.size(), hash.data());
+  std::string url = tracker + '?';
+  url += "info_hash=";
+  char* encoded_info_hash = curl_easy_escape(
+      curl, reinterpret_cast<const char*>(hash.data()), SHA_DIGEST_LENGTH);
+  url += std::string(encoded_info_hash);
+  url += "&peer_id=";
+  url += peer_id;
+  url += "&port=";
+  url += std::to_string(port);
+  url += "&uploaded=";
+  url += std::to_string(uploaded);
+  url += "&downloaded=";
+  url += std::to_string(downloaded);
+  url += "&left=";
+  url += std::to_string(left);
+  url += "&compact=";
+  url += std::to_string(compact);
+  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+  curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+  curl_easy_setopt(curl,CURLOPT_WRITEDATA, &response);
+  CURLcode status = curl_easy_perform(curl);
+  if (status != CURLE_OK) {
+    std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(status) << std::endl;
+  }
+  curl_free(encoded_info_hash);
+  curl_easy_cleanup(curl);
+  std::cout << decode_bencoded_value(response).dump();
 }
 
 int main(int argc, char* argv[]) {
@@ -234,9 +308,10 @@ int main(int argc, char* argv[]) {
     }
     std::string file = argv[2];
     std::vector<std::string> info = parse_torrent_file(file);
-    for (const auto& i : info) {
-      std::cout << i << '\n';
-    }
+//    for (const auto& i : info) {
+//      std::cout << i << '\n';
+//    }
+    send_request(info);
   } else {
     std::cerr << "unknown command: " << command << std::endl;
     return 1;
